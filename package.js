95cc1525e2002fc5b22fdce8fff493e682784312
Miles <milesd92@msn.com>
2017-01-25 17:37:41 -0600

    Update package.js

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
-	name: "mrt:bootstrap-growl"
+	name: "kilometers:bootstrap-growl"
 	, version: "0.1.4"
     , summary: "Pretty simple jQuery plugin that turns standard Bootstrap alerts into Growl-like notifications."
 });
 
-Package.on_use(function (api) {
+Package.onUse(function (api) {
     api.use('jquery@1.0.11', 'client');
-    api.add_files([
+    api.addFiles([
         'lib/jquery.bootstrap-growl.js'
     ], 'client');
 });
